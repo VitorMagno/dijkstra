48ce95fe519dfc8e942610f7a392bdb9751f294b
Vitor Magno C. de Gouveia <45520686+VitorMagno@users.noreply.github.com>
2022-06-10 18:54:57 -0300

    nothing important

--- main.cpp
+++ main.cpp
@@ -12,11 +12,12 @@ int main(int argc, char const *argv[])
     }
     if (argv[1] != "-f"){
         cout << "invalid parameter, start with -f and set a input file " << argv[1] << endl;
-        //nao ta reconhecendo o parametro -f resolver
+        //nao ta reconhecendo o parametro -f (resolver)
         return 0;
     }
     fstream entrada;
     string linhas, v, e;
+    
     // colocar a primeira linha que contem a quantidade de vertices e arestas num Vetor e em seguida em
     //duas variaveis V e E 
     entrada.open("exemplo.txt", ios::in);
